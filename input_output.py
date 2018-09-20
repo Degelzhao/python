c0c1e590162db90ad5342e44dac6fcb7480a3bbc
degel·zhao <barryember@outlook.com>
2018-09-20 17:27:51 +0800

    practice input and output action

---
+++ input_output.py
@@ -0,0 +1,10 @@
+# input/output exercises
+str1 = input('please input num1: ')
+num1 = int(str1)
+
+str2 = input('please input num2: ')
+num2 = int(str2)
+
+print('sum: %d + %d = %d'%(num1,num2,num1 + num2))
+
+#tips:当你使用input()时，获取的内容都是str(字符串)类型。
