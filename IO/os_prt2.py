0c9d4f3a19c3a347e4251bf779070bf8f07506fe
degel·zhao <barryember@outlook.com>
2018-10-06 21:41:50 +0800

    OS module

--- IO/os_prt2.py
+++ IO/os_prt2.py
@@ -1,3 +1,7 @@
+'os module'
+
+__author__ = 'Degel zhao'
+
 #编写一个程序，能在当前目录以及当前目录的所有子目录下查找文件名包含指定字符串的文件，并打印出相对路径
 
 import os
@@ -12,10 +16,10 @@ def check(up, keyword):
 		try:
 			abs_x = os.path.join(up, x)     #拼接路径
 			if os.path.isdir(abs_x):        #判断abs_x是否是已经存在的目录
-				up = os.path.join(up, x)    
+				up = os.path.join(up, x)    #存在的话，拼接路径
 				check(up, keyword)
 				up = os.path.split(up)[0]   #将地址分割成目录和文件名返回
-			if os.path.isfile(abs_x):       #判断abs_x是否是已经存在的目录  
+			if os.path.isfile(abs_x):       #判断abs_x是否是文件  
 				if keyword in x:            #判断关键字是否在路径当中
 					result.append(abs_x)    #追加路径
 
@@ -23,7 +27,9 @@ def check(up, keyword):
 			continue
 
 check(up, keyword)
+#打印出路径的总条数
 print('\n==========Find %d files==========\n' % len(result))
+#循环将找到的路径打印出来
 num = 0
 for r in result:
 	num += 1
